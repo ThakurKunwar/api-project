23bd74e8e7f71664028de4ab552328a982d4a69e
ThakurKunwar <thakurkunwar10@gmail.com>
2026-04-27 15:07:20 +0545

    create post CRUD with Resources

---
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return PostResource::collection(Post::with('user')->paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+    {
+        //
+        $data = $request->validated();
+        $data['user_id'] = 1;
+
+        $post = Post::create($data);
+        return response()->json(
+            [new PostResource($post)],
+            201
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+        $post = Post::findOrFail($id);
+        return (new PostResource($post));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+        $data =  $request->validate([
+            'title' => ['required', 'min:4'],
+            'body' => ['required', 'string', 'min:10'],
+        ]);
+
+        $post = Post::findOrFail($id);
+        $post->update($data);
+        return (new PostResource($post));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+        return response()->noContent();
+    }
+}
